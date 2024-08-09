b5ce0d000c3977545aae5f24d9f192e4a8377f06
D070508T <davidt070508@gmail.com>
2024-08-09 12:43:34 -0400

    Base

--- Board.py
+++ Board.py
@@ -1,70 +1,50 @@
 class Board:
     # Constructor that builds the board and sets it to empty
     def __init__(self, board=None):
-        self.winning_combinations = (
-            (0, 1, 2),  # Row 1
-            (3, 4, 5),  # Row 2
-            (6, 7, 8),  # Row 3
-            (0, 3, 6),  # Column 1
-            (1, 4, 7),  # Column 2
-            (2, 5, 8),  # Column 3
-            (0, 4, 8),  # Diagonal 1
-            (2, 4, 6)   # Diagonal 2
-        )
-
         if board:
-            self.board = board.board
+            self.board = [row[:] for row in board.board]
         else:
-            self.board = '         '
-
-    def rotate(self):
-        return (self.board[6] + self.board[3] + self.board[0] +
-                self.board[7] + self.board[4] + self.board[1] +
-                self.board[8] + self.board[5] + self.board[2])
-
-    def mirror(self, num):
-        if num % 2 == 0:
-            return self.board[6:9] + self.board[3:6] + self.board[0:3]
-        return (self.board[2] + self.board[1] + self.board[0] +
-                self.board[5] + self.board[4] + self.board[3] +
-                self.board[8] + self.board[7] + self.board[6])
-
-    def variations(self):
-        variations = []
-
-        newBoard = Board(self)
-
-        for i in range(3):
-            newBoard.mirror(i)
-            variations.append(Board(newBoard))
-            for j in range(3):
-                newBoard.rotate()
-                variations.append(Board(newBoard))
-
-        return variations
+            self.board = [
+                [' ', ' ', ' '],
+                [' ', ' ', ' '],
+                [' ', ' ', ' ']
+            ]
 
     # Function that takes a number and a character and places that character in the correct spot of the given number
     def place(self, num, char):
-        oldBoard = self.board
+        self.board[(num - 1) // 3][(num - 1) % 3] = char
 
-        self.board = self.board[:num - 1] + char
-
-        if num < 9:
-            self.board += oldBoard[num:]
+    # Function that returns the value inside the spot of the given number
+    def get(self, n):
+        return self.board[(n - 1) // 3][(n - 1) % 3]
 
     # Function that returns the state of the board. Who won, or if the game is still continuing, or if it's a tie
     def state(self):
-        for a, b, c in self.winning_combinations:
-            if self.board[a] == self.board[b] == self.board[c] and self.board[a] != ' ':
-                return self.board[a]  # Return 'X' or 'O'
+        for i in range(3):
+            # Check row
+            if self.board[i][0] != ' ' and self.board[i][0] == self.board[i][1] and self.board[i][1] == self.board[i][2]:
+                return self.board[i][0]
 
-        # If no winner, check if the board is full, meaning it's a tie, or if the game is still on
-        if ' ' not in self.board:
-            return 'tie'
-        return 'continue'
+            # Check column
+            if self.board[0][i] != ' ' and self.board[0][i] == self.board[1][i] and self.board[1][i] == self.board[2][i]:
+                return self.board[0][i]
+
+        # Check diagonals
+        if self.board[0][0] != ' ' and self.board[0][0] == self.board[1][1] and self.board[1][1] == self.board[2][2]:
+            return self.board[0][0]
+
+        if self.board[0][2] != ' ' and self.board[0][2] == self.board[1][1] and self.board[1][1] == self.board[2][0]:
+            return self.board[0][2]
+
+        for i in range(3):
+            for j in range(3):
+                if self.board[i][j] == ' ':
+                    return 'continue'
+        return 'tie'
 
     # Function that checks if a board is unique
     def unique(self, boards):
-        if self.board in boards:
-            return False
+        for other in boards:
+            if self.board == other.board:
+                return False
         return True
